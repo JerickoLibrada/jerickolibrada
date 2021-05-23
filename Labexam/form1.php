8fb8949dea6dcd54ee0add9af7cf179efcb2c144
Jericko16 <jericko@gmail.com>
2021-05-23 14:41:01 +0800

    Version 1 Librada Jericko J

---
+++ Labexam/form1.php
@@ -0,0 +1,13 @@
+<html>
+<head><title>Sent</title></head>
+
+<body>
+<h1>File Saved!</h1>
+<a href = "index.php">create again?</a>
+<br>
+<p>You can save your work via direct download!</p>
+<form method="get" action="index.php">
+   <button type="submit">Download!</button>
+</form>
+</body>
+</html>
---
+++ Labexam/index.php
@@ -0,0 +1,101 @@
+<html>
+<head><title>Ikoy</title></head>
+<body>
+
+<table align="center" width="80%" border="2">
+	<tr>
+		<th><a href="index.php" style="font-size:20px">Biodata</a></th>
+	</tr></table>	
+<form>
+	<center>
+	<fieldset style = "width:80% ">
+<div class = "PersoanalData">
+
+	<center>
+		<br>
+		<br>
+		<font style="font-size:15px">Personal Data</font> 
+	<table>
+		<center>
+    	<tr>
+        	<td>&nbsp;</td>
+    	</tr>
+
+   		 <tr>
+				   		 	
+       		<td>Firstname:         	        <input type="text" name="id" value="" required>
+       			
+       		<td>Age: <input type="text" name="id" value="">
+
+       			<br>
+       			<br>
+	    </tr>
+
+	    <tr>
+	        <td>Middlename:<input type="text" name="id" value="" /></td>
+	        	
+	        <td>Birthplace: <input type="text" name="id" value="">
+	        	<br>
+	        	<br>
+	    </tr>
+
+	    <tr>
+	        <td>Lastname:<input type="text" name="id" value="" /></td>
+	        	
+	        <td>Birthday: <input type="date" name="id" value="">
+	        	<br>
+	        	<br>
+	    </tr>
+	    <br>
+
+	</table>
+
+</div>
+<br>
+<div>
+	<center>
+		<br>
+		<br>
+		<font style="font-size:15px">Educational attainment</font> 
+		<br>
+		<br>
+		<br>
+	 <tr>
+       		<td>Elementary :<input type="text" name="id" value="">
+       			
+       		<td>Year Graduated: <input type="text" name="id" value="">
+       			<br>
+       			<br>
+	 </tr>
+
+	  <tr>
+       		<td>High School :<input type="text" name="id" value="">
+       			
+       		<td>Year Graduated: <input type="text" name="id" value="">
+       			<br>
+       			<br>
+	 </tr>
+
+	  <tr>
+       		<td>College :<input type="text" name="id" value="">
+       			
+       		<td>Year Graduated: <input type="text" name="id" value="" required>
+       			<br>
+	 </tr>
+</div>
+<div>
+	<center>
+		<br>
+		<br>
+		<font style="font-size:15px">Interests</font> 
+		<br>
+		
+		<textarea name="comment" form="usrform">Enter text here...</textarea>
+
+		<a href="form1.php"><input type = "button" name="Submit" value="submit"></a>
+
+</div>
+</form>
+	</fieldset>
+</body>
+</html>
